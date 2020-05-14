9a266e40092bb92b59b39e6c7cb309bcb84ea9fe
zhushen <137895356@qq.com>
2020-05-14 11:40:13 +0800

    遗漏的提交

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,11 +1,41 @@
 <template>
   <div class="todo-header">
-        <input type="text" placeholder="请输入你的任务名称，按回车键确认"/>
+        <input type="text" placeholder="请输入你的任务名称，按回车键确认"
+        v-model="title" @keyup.enter="addItem"/>
       </div>
 </template>
 
 <script>
 export default {
+  props: {
+    addTodo: Function
+  },
+
+  data () {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    addItem () {
+      /** 1、检查输入的数据的合法性 */
+      const title = this.title.trim()
+      if (!title) {
+        alert('必须输入')
+        return
+      }
+      /** 2、根据输入生成一个todo对象 */
+      const todo = {
+        title,
+        complete: false
+      }
+      /** 3、将todo对象添加到todos  */
+      this.addTodo(todo)
+      /** 4、清除输入  */
+      this.title = ''
+    }
+  }
+
 }
 </script>
 
