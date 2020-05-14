1b96cb85ead2aadeb67098bd27fe6f89823e9987
zhushen <137895356@qq.com>
2020-05-14 10:30:23 +0800

    添加组件遍历显示页面

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+<div class="todo-container">
+    <div class="todo-wrap">
+      <todo-header/>
+      <todo-list :todos="todos"/>
+      <todo-footer/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import TodoHeader from './components/TodoHeader.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
 
 export default {
-  name: 'App',
+  data () {
+    return {
+      todos: [
+        {title: '吃饭', complete: false},
+        {title: '睡觉', complete: true},
+        {title: '打豆豆', complete: false}
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    TodoHeader,
+    TodoList,
+    TodoFooter
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
 }
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,39 @@
+<template>
+   <ul class="todo-main">
+      <TodoItem v-for="(todo,index) in todos" :todo="todo" :key="index" :index="index"/>
+      </ul>
+</template>
+
+<script>
+import TodoItem from './TodoItem'
+export default {
+  props: {
+    todos: Array
+  },
+  components: {
+    TodoItem
+  }
+}
+
+</script>
+
+<style>
+
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+
+</style>
